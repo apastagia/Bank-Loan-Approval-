696a4959eadd083e4e6e7cb25f3fc63a4b9ae8c6
AayushiPastagia <53651953+AayushiPastagia@users.noreply.github.com>
2021-05-06 12:31:03 +0530

    modified views.py

--- DjangoAPI/MyAPI/views.py
+++ DjangoAPI/MyAPI/views.py
@@ -9,6 +9,7 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import approvals
 from .serializers import approvalsSerializers
+from keras.models import model_from_json
 import pickle
 import joblibs
 import json
@@ -24,3 +25,26 @@ class ApprovalsView(viewsets.ModelViewSet):
 def myform(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit = False)
+            #myform.save()
+    else:
+        form = MyForm()
+    #return render(request,'myform/form.html',{'form':form})
+@api_view(['POST'])
+def approvereject(request):
+    try:
+        mdl = joblib.load('loan_model.pkl')
+        mydata = request.data
+        unit = np.array(list(mydata.values()))
+        unit = unit.reshape(1,-1)
+        scalers = joblib.load('scalers.pkl')
+        X = scalers.transform(unit)
+        y_pred = mdl.predict(X)
+        y_pred = (y_pred > 0.58)
+        newdf = pd.DataFrame(y_pred, columns = ['Status'])
+        newdf = newdf.replace({True:'Approved', False:'Rejected'})
+        return JsonResponse('Your Status is {}'.format(newdf), safe=False)
+
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
